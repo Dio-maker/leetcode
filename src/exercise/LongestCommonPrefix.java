fe07a2b6a11b19ea2ccb36bd59f12500f2c7243e
wmk_HS <1670086269@qq.com>
2023-05-06 17:46:01 +0800

    leetcode

--- src/exercise/LongestCommonPrefix.java
+++ src/exercise/LongestCommonPrefix.java
@@ -1,19 +1,45 @@
 package exercise;
 
-import java.util.Iterator;
-import java.util.LinkedHashSet;
+
+import java.util.HashSet;
 
 public class LongestCommonPrefix {
 
     public static void main(String[] args) {
-        String[] strings = {"a"};
+        String[] strings = {"adasdsac","adasds","adasdac"};
         System.out.println(longestCommonPrefix(strings));
     }
 
     public static String longestCommonPrefix(String[] strs) {
+        boolean flag = true;
+        StringBuilder builder = new StringBuilder();
+        int len = 0;
+        HashSet<Character> hashSet = new HashSet<>();
+        if (strs.length<=1){
+            return strs[0];
+        }else {
+            len = strs[0].length();
+        }
+        for (int i = 0; i < strs.length; i++) {
+            len = Math.min(strs[i].length(), len);
+        }
+        int j = 0;
+
+        while (j<len){
+            for (int i = 0; i < strs.length; i++) {
+                hashSet.add(strs[i].charAt(j));
+            }
+            if (hashSet.size()<=1){
+                builder.append(hashSet.iterator().next());
+            }else {
+                break;
+            }
+            hashSet.clear();
+            j++;
+        }
 
 
-        return null;
+        return builder.toString();
     }
 }
 
---
+++ src/exercise/ThreeSum.java
@@ -0,0 +1,16 @@
+package exercise;
+
+import java.util.List;
+
+public class ThreeSum {
+
+    public static void main(String[] args) {
+
+    }
+    public List<List<Integer>> threeSum(int[] nums) {
+
+
+        return null;
+    }
+
+}
